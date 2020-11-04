e67d317a05c4800601840a9fc5815696ca98d6c8
mikamo3 <kamo3proj@gmail.com>
2020-11-04 13:40:29 +0000

    refactoring

--- old/host_vars/lint_init.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/env bash
-set -x
-if [[ "$1" != "" ]]; then
-  TAGS="--tags=$1"
-  shift
-fi
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_local.yml playbook_init.yml $TAGS --vault-password-file=".pass" --syntax-check $@
--- old/host_vars/run_init.sh
+++
@@ -1,2 +0,0 @@
-#/usr/bin/env bash
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_local.yml playbook_init.yml tags=init --ask-vault-pass
