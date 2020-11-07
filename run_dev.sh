103e3c581bb030d254ce10cc0fa07820c445a8e6
mikamo3 <kamo3proj@gmail.com>
2020-11-07 16:25:15 +0000

    :rocket: (add production configure)

--- run_dev.sh
+++ run_dev.sh
@@ -4,4 +4,4 @@ if [[ "$1" != "" ]]; then
   TAGS="--tags=$1"
   shift
 fi
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_remote.yml playbook.yml $TAGS --vault-password-file=".pass" --extra-vars="@password.yml" $@
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_remote_dev.yml playbook.yml $TAGS --vault-password-file=".pass" --extra-vars="@password.yml" $@
