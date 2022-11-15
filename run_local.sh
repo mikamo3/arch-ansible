d81cd6270468dee39d43e33a281a7650f4ad4848
mikamo3 <kamo3proj@gmail.com>
2022-11-15 20:34:40 +0900

    :sparkles: (scripts) Add extra-vars

--- run_local.sh
+++ run_local.sh
@@ -4,4 +4,4 @@ if [[ "$1" != "" ]]; then
   TAGS="--tags=$1"
   shift
 fi
-ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_local.yml playbook.yml $TAGS --vault-password-file=".pass" $@
+ANSIBLE_CONFIG=./ansible.cfg ansible-playbook -i hosts_local.yml playbook.yml $TAGS --vault-password-file=".pass"  --extra-vars="@password.yml" $@
